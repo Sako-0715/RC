0298cf29f83be762547a224fce0a37ddf12ce93a
酒匂 竜也 <tatsuya_sako@hot-factory.jp>
2024-08-28 13:26:04 +0900

    Initial Commit

---
+++ RC/RCApp.swift
@@ -0,0 +1,17 @@
+//
+//  RCApp.swift
+//  RC
+//
+//  Created by HFFM-012 on 2024/08/28.
+//
+
+import SwiftUI
+
+@main
+struct RCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
